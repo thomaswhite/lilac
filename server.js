bed79f28f338575ebe6c83d45545e197173e2b94
Thomas White <thomas.0007@gmail.com>
2013-06-04 06:15:56 +0100

    Update to ise linkedin dust module and express static

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var express = require('express');
-var dust = require('dust');
+var dust = require('dustjs-linkedin');
 var Models = require('./models/models');
 var Collections = require('./models/collections');
 var lilac = require('./lib/lilac');
+var path = require('path');
+
 require('./lib/watcher').watch(dust, './templates', './public/templates', '.jst');
 
 var app = express.createServer();
 app.use(express.cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(8124);
 
 lilac.initialize(app);
 
 // Static resources
+/*
 app.get('/public/*.(js|css)', function(req, res, next) {
 	res.sendfile('./' + req.url);
 });
+*/
 
 app.get('/', function(req, res, next) {
 	var collection = new Collections.DelayedCollection([
